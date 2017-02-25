57138d639f0abe3977e17a6b109207acf151d6c9
killvak <killvak@live.com>
2017-02-25 13:35:50 +0200

    First commit

--- Ajman DED/ViewController.swift
+++ Ajman DED/ViewController.swift
@@ -8,18 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIWebViewDelegate {
 
+    @IBOutlet weak var pageTitle: UILabel!
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var reloadBtnOL: UIButton!
+    @IBOutlet weak var backBtnOL: UIButton!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.view.squareLoading.backgroundColor = UIColor(red: 255/255, green: 254/255, blue: 224/255, alpha: 1)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        webView.delegate = self
+        // Do any additional setup after loading the view, typically from a nib.  http://ajmanded.ae/Default.aspx
+       // self.view.squareLoading.start(0.0)
+        let url = URL(string: "http://ajmanded.ae/Default.aspx")
+        if let url = url {
+        let request = URLRequest(url: url  )
+        webView.loadRequest(request)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+ 
+ 
+    @IBAction func refreshBtnAct(_ sender: UIButton) {
+        webView.reload()
+    }
+    @IBAction func backbtnAct(_ sender: UIButton) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    func webViewDidStartLoad(_ webView: UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+        print("started Loading")
+        self.buttonVisibility(bool: webView.canGoBack)
+        self.view.squareLoading.start(0.0)
+    }
+    
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        print("finished loading")
+       
+        self.buttonVisibility(bool: webView.canGoBack)
+        self.pageTitle.text = webView.stringByEvaluatingJavaScript(from: "document.title")
+        self.view.squareLoading.stop(0.0)
     }
 
+    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        print("failed")
+    }
+    
+    func buttonVisibility(bool : Bool) {
+        
+        if bool {
+            self.backBtnOL.isHidden = false
 
+        }else {
+            self.backBtnOL.isHidden = true
+        }
+        self.reloadBtnOL.isHidden = false
+
+    }
+
+    
+    
 }
 
